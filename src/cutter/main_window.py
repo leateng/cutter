b473ca9be5b4265efa2679be64e796acf982b6ed
teng.li <teng.li@yhlsoft.com>
2023-04-06 21:51:54 +0800

    update

--- src/cutter/main_window.py
+++ src/cutter/main_window.py
@@ -1,13 +1,17 @@
-from PySide6.QtGui import QAction
-from PySide6.QtWidgets import QMainWindow, QFileDialog, QMessageBox
+from PySide6.QtCore import QSize
+from PySide6.QtGui import QAction, QIcon, QPixmap
+from PySide6.QtWidgets import QMainWindow, QFileDialog, QMessageBox, QToolBar
 import ezdxf
 from ezdxf.lldxf.const import DXFStructureError
 from cutter.cad_widget import CADGraphicsView, DxfEntityScence
+import cutter.rc_images
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self._init_toolbar()
         self.view = CADGraphicsView()
         self.scene = DxfEntityScence([])
         self.view.setScene(self.scene)
@@ -18,6 +22,18 @@ class MainWindow(QMainWindow):
         select_doc_action = QAction("File", self)
         select_doc_action.triggered.connect(self._select_doc)
         menu.addAction(select_doc_action)
+
+    def _init_toolbar(self):
+        toolbar = QToolBar("My main toolbar")
+        toolbar.setIconSize(QSize(32, 32))
+        toolbar.setMovable(False)
+        self.addToolBar(toolbar)
+
+        button_action = QAction(QIcon(QPixmap(":/images/start.png")), "启动机器", self)
+        button_action.setStatusTip("This is your button")
+        button_action.triggered.connect(self.onStartCutter)
+        button_action.setCheckable(False)
+        toolbar.addAction(button_action)
 
     def _select_doc(self):
         path, _ = QFileDialog.getOpenFileName(
@@ -50,3 +66,6 @@ class MainWindow(QMainWindow):
         self.scene = DxfEntityScence(entities)
         self.view.setScene(self.scene)
         self.view.fit_to_scene()
+
+    def onStartCutter(self):
+        print("start machine")
