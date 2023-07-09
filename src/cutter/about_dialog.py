4f4fe5456f2c2e1bccce1f03de99007d6044cad6
leateng <leateng@foxmail.com>
2023-07-09 11:57:12 +0800

    update

--- src/cutter/about_dialog.py
+++ src/cutter/about_dialog.py
@@ -1,8 +1,9 @@
 from qtpy.QtCore import Qt
-from qtpy.QtGui import QPixmap
+from qtpy.QtGui import QPixmap, QIcon
 from qtpy.QtWidgets import QDialog, QFormLayout, QVBoxLayout, QLabel, QPushButton
 import qtawesome as qta
 from cutter.consts import VERSION
+
 
 class AboutUsDialog(QDialog):
     def __init__(self, parent=None):
@@ -21,19 +22,27 @@ class AboutUsDialog(QDialog):
 
         # 联系方式
         address_logo_label = QLabel()
-        address_logo_label.setPixmap(qta.icon("ei.map-marker", color="white").pixmap(20, 20))
-        address_label = QLabel("烟台总部：中国（山东）自贸区烟台片区长江路300号业达智谷\n华北研发中心：河北省保定市涿州市高新技术产业开发区和谷科技产业园B12楼\n西北研发中心：陕西省西安市未央区草滩生态产业园弘业一路远征科技园1幢3楼")
+        address_logo_label.setPixmap(
+            qta.icon("ei.map-marker", color="white").pixmap(20, 20)
+        )
+        address_label = QLabel(
+            "烟台总部：中国（山东）自贸区烟台片区长江路300号业达智谷\n华北研发中心：河北省保定市涿州市高新技术产业开发区和谷科技产业园B12楼\n西北研发中心：陕西省西安市未央区草滩生态产业园弘业一路远征科技园1幢3楼"
+        )
 
-        email_logo_label= QLabel()
+        email_logo_label = QLabel()
         email_logo_label.setPixmap(qta.icon("mdi.email", color="white").pixmap(20, 20))
         email_label = QLabel("邮箱：nancyzhang@pcm-bj.com")
-        
+
         phone_logo_label = QLabel()
-        phone_logo_label.setPixmap(qta.icon("ri.phone-fill", color="white").pixmap(20, 20))
+        phone_logo_label.setPixmap(
+            qta.icon("ri.phone-fill", color="white").pixmap(20, 20)
+        )
         phone_label = QLabel("电话：0535-6388095")
-        
+
         form_layout = QFormLayout()
-        form_layout.setLabelAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignRight)
+        form_layout.setLabelAlignment(
+            Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignRight
+        )
         form_layout.addRow(address_logo_label, address_label)
         form_layout.addRow(email_logo_label, email_label)
         form_layout.addRow(phone_logo_label, phone_label)
@@ -48,4 +57,5 @@ class AboutUsDialog(QDialog):
         layout.addLayout(form_layout)
         layout.addWidget(copyright_label)
 
+        self.setWindowIcon(QIcon(QPixmap(":/images/info.png")))
         self.setLayout(layout)
